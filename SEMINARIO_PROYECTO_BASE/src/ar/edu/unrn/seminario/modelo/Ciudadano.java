53a7a33ffb44b006beb78cc6867f991b992b0fd0
KarenRuizAiza <70164086+KarenRuizAiza@users.noreply.github.com>
2021-09-03 19:53:35 -0300

    Create Ciudadano.java
    agregue clase ciudadano

---
+++ SEMINARIO_PROYECTO_BASE/src/ar/edu/unrn/seminario/modelo/Ciudadano.java
@@ -0,0 +1,71 @@
+package ar.edu.unrn.seminario.modelo;
+
+public class Ciudadano {
+
+	private String nombre;
+	private String apellido;
+	private String dni;
+	
+	public Ciudadano (String nombre, String apellido, String dni) {
+		
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.dni = dni;
+	} 	
+ 
+	public String nombre() {
+		return nombre;
+	}
+	public void editarNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String apellido() {
+		return apellido;
+	}
+	public void editarApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String dni() {
+		return dni;
+	}
+	public void editarDni(String dni) {
+		this.dni = dni;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((apellido == null) ? 0 : apellido.hashCode());
+		result = prime * result + ((dni == null) ? 0 : dni.hashCode());
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Ciudadano other = (Ciudadano) obj;
+		if (apellido == null) {
+			if (other.apellido != null)
+				return false;
+		} else if (!apellido.equals(other.apellido))
+			return false;
+		if (dni == null) {
+			if (other.dni != null)
+				return false;
+		} else if (!dni.equals(other.dni))
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		return true;
+	}
+}
