327439878713dd23061ebddc82320afaa5afb0f2
KarenRuizAiza <70164086+KarenRuizAiza@users.noreply.github.com>
2021-09-06 17:51:37 -0300

    Create NumbersException.java
    excepciones

---
+++ SEMINARIO_PROYECTO_BASE/src/ar/edu/unrn/seminario/exception/NumbersException.java
@@ -0,0 +1,13 @@
+package ar.edu.unrn.seminario.exception;
+
+public class NumbersException extends Exception{
+
+	public NumbersException () {
+		
+	}
+	
+	
+	public NumbersException (String message) {
+		super(message);
+	}
+}
